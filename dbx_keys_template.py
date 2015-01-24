e28b22482c2ea3d383939b0be70c3039a574281e
James Porter <james@amimetic.co.uk>
2015-01-24 11:24:10 +0000

    added post method for webhooks and notes on how it should work

---
+++ dbx_keys_template.py
@@ -0,0 +1,6 @@
+#Create a dbx_keys.py file with these filled in
+
+dbx_keys = {
+    "app_key": "",
+    "app_secret": ""
+}
--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import webapp2
 import jinja2
 import os
+import dbx_keys
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates/"))
@@ -19,6 +20,22 @@ class HooksHandler(webapp2.RequestHandler):
     def get(self):
         """Respond to the webhook verification (GET request) by echoing back the challenge parameter."""
         self.response.out.write(self.request.get('challenge'))
+
+    def post(self):
+        """The webhook message: should have a json body which looks like:
+        {
+           "delta": {
+                "users": [
+                    12345678,
+                    23456789,
+                    ...
+                ]
+            }
+        }
+
+        should then call /delta (in core Dbx API) with a path prefix
+        """
+        pass
 
 
 app = webapp2.WSGIApplication([
